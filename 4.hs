3393a9a0f820eab8b771c8edba8be6c253f6021e
Ben Hall <ben.hall@crunch.co.uk>
2015-12-23 20:51:04 +0000

    use maximum

--- 4.hs
+++ 4.hs
@@ -1,4 +1,6 @@
+import Data.List (maximum)
+
 main = putStrLn . show $ largestPalindrome 3
-largestPalindrome n = foldl1 max [a * b | a <- nDigitNumbers n, b <- nDigitNumbers n, isPalindrome $ a * b]
+largestPalindrome n = maximum [a * b | a <- nDigitNumbers n, b <- nDigitNumbers n, isPalindrome $ a * b]
 isPalindrome n = show n == reverse (show n)
 nDigitNumbers n = [0..10 ^ n - 1]
