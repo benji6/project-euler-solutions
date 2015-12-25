0058a5b6be7e4884c5359cde1d8ff3b7f75a474e
benji6 <benhall22@hotmail.co.uk>
2015-12-25 17:59:44 +0000

    js solution 9

---
+++ javascript/9.js
@@ -0,0 +1,15 @@
+const pythagoreanTriplets = function * () {
+  for (var a = 1; a < 998; a++) {
+    for (var b = 1; b < 999 - a; b++) {
+      const c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2))
+      if (Math.pow(Math.round(c), 2) === Math.pow(a, 2) + Math.pow(b, 2)) yield [a, b, c]
+    }
+  }
+}
+
+for (const x of pythagoreanTriplets()) {
+  if (x.reduce((a, b) => a + b) === 1000) {
+    console.log(x.reduce((a, b) => a * b))
+    break
+  }
+}
