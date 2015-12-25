64d813a772a4f1f2a850544a9e0dad1145c52af1
benji6 <benhall22@hotmail.co.uk>
2015-12-25 16:59:01 +0000

    js solution 7

---
+++ javascript/7.js
@@ -0,0 +1,13 @@
+const primes = function * () {
+  const xs = []
+  let x = 2
+  while (xs.length < 10001) {
+    if (xs.every(y => x % y !== 0)) {
+      xs.push(x)
+      yield x
+    }
+    x++
+  }
+}
+
+console.log([...primes()].pop())
