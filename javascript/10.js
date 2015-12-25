d958a8e575087b84058d4990b8c3b4f5a20d021f
benji6 <benhall22@hotmail.co.uk>
2015-12-25 18:05:17 +0000

    js solution 10

---
+++ javascript/10.js
@@ -0,0 +1,15 @@
+const primes = function * () {
+  const xs = []
+  let x = 2
+  while (true) {
+    if (xs.every(y => x % y !== 0)) {
+      xs.push(x)
+      yield x
+    }
+    x++
+  }
+}
+let sum = 0
+for (const x of primes()) if (x >= 2000000) break; else sum += x
+
+console.log(sum)
